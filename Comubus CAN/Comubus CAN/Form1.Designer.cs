c754dfaeafd66565eb3778c0f672a3d2bb9713eb
hanifryanas <hanifryanas@gmail.com>
2018-10-08 21:25:21 +0700

    Add files via upload

---
+++ Comubus CAN/Comubus CAN/Form1.Designer.cs
@@ -0,0 +1,241 @@
+﻿namespace Comubus_CAN
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.label4 = new System.Windows.Forms.Label();
+            this.button5 = new System.Windows.Forms.Button();
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.panel1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(534, 27);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 28);
+            this.comboBox1.TabIndex = 0;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.textBox1);
+            this.panel1.Controls.Add(this.label3);
+            this.panel1.Location = new System.Drawing.Point(23, 79);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(733, 349);
+            this.panel1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(227, 4);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 20);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Data";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(0, 27);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(733, 319);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(3, 4);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(87, 20);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Timestamp";
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.LightGray;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.LightGray;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(22, 51);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 29);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Raw Data";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.LightGray;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.LightGray;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(94, 51);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 29);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Time Base";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.LightGray;
+            this.button3.FlatAppearance.BorderColor = System.Drawing.Color.LightGray;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button3.Location = new System.Drawing.Point(167, 51);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 29);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "ID Base";
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(415, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(116, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Select COM >>";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(23, 435);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(611, 40);
+            this.textBox2.TabIndex = 6;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(640, 436);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(115, 40);
+            this.button4.TabIndex = 7;
+            this.button4.Text = "Send";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 6F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(432, 58);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(84, 15);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "not connected";
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(661, 25);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(94, 33);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "Connect";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // backgroundWorker2
+            // 
+            this.backgroundWorker2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker2_DoWork);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Gray;
+            this.ClientSize = new System.Drawing.Size(787, 499);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.comboBox1);
+            this.Name = "Form1";
+            this.Text = "Comubus CAN";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button button5;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+    }
+}
+
---
+++ Comubus CAN/Comubus CAN/Form1.cs
@@ -0,0 +1,283 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Comubus_CAN
+{
+    public partial class Form1 : Form
+    {
+        int formnya = 0;
+        string bawahnya = System.Environment.NewLine;
+        public string bufftext;
+        public string[] oo2;
+        public int oo3;
+        public int oo4;
+        public int iter = 0;
+        public int[] intbufftext;
+        public string result3 = string.Empty;
+        public Form1()
+        {
+            InitializeComponent();
+            label2.Visible = false;
+            label3.Visible = false;
+            textBox1.ScrollBars = ScrollBars.Vertical;
+            foreach (String oo in SerialPort.GetPortNames()) {
+                comboBox1.Items.Add(oo);
+            }
+        }
+        SerialPort seport;
+        public void serial_port_connect(String port) {
+            DateTime dt = DateTime.Now;
+            String dtn = dt.ToShortTimeString();
+
+            seport = new SerialPort(port);
+        }
+            /*try
+            {
+                label4.Text = "Connected to" + port;
+                seport.DataReceived += new SerialDataReceivedEventHandler(seport_DataReceived);
+            }
+            catch (Exception ex) {
+                MessageBox.Show(ex.ToString(), "Error");
+            }
+        }
+        private void seport_DataReceived(object sender, SerialDataReceivedEventArgs e) {
+            DateTime dt = DateTime.Now;
+            String dtn = dt.ToShortTimeString();
+            textBox1.AppendText( dtn + "\t" + seport.ReadExisting() + "\n");
+        }*/
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Control ctrl = ((Control)sender);
+            switch (ctrl.BackColor.Name) {
+                case "LightGray":
+                    ctrl.BackColor = Color.Gray;
+                    formnya = 1;
+                    label3.Visible = true;
+                    label3.Text = "Data";
+                    label2.Text = "";
+                    textBox1.Text = result3;
+                    button2.BackColor = Color.LightGray;
+                    button3.BackColor = Color.LightGray;
+                    break;
+                case "Gray":
+                    ctrl.BackColor = Color.LightGray;
+                    formnya = 0;
+                    label3.Visible = false;
+                    textBox1.Text = "";
+                    break;
+            }
+           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Control ctrl = ((Control)sender);
+            switch (ctrl.BackColor.Name)
+            {
+                case "LightGray":
+                    ctrl.BackColor = Color.Gray;
+                    formnya = 2;
+                    label3.Visible = true;
+                    label2.Visible = true;
+                    label3.Text = "Timestamp";
+                    label2.Text = "Data";
+                    button1.BackColor = Color.LightGray;
+                    button3.BackColor = Color.LightGray;
+                    break;
+                case "Gray":
+                    ctrl.BackColor = Color.LightGray;
+                    formnya = 0;
+                    label3.Visible = false;
+                    label2.Visible = false;
+                    break;
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Control ctrl = ((Control)sender);
+            switch (ctrl.BackColor.Name)
+            {
+                case "LightGray":
+                    ctrl.BackColor = Color.Gray;
+                    formnya = 3;
+                    button1.BackColor = Color.LightGray;
+                    button2.BackColor = Color.LightGray;
+                    break;
+                case "Gray":
+                    ctrl.BackColor = Color.LightGray;
+                    formnya = 0;
+                    break;
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+        public void konversiabjadhexa1()
+        {
+            if (oo2[0] == "A") { oo3 = 10; }
+            if (oo2[0] == "B") { oo3 = 11; }
+            if (oo2[0] == "C") { oo3 = 12; }
+            if (oo2[0] == "D") { oo3 = 13; }
+            if (oo2[0] == "E") { oo3 = 14; }
+            if (oo2[0] == "F") { oo3 = 15; }
+        }
+        public void konversiabjadhexa2()
+        {
+            if (oo2[1] == "A") { oo4 = 10; }
+            if (oo2[1] == "B") { oo4 = 11; }
+            if (oo2[1] == "C") { oo4 = 12; }
+            if (oo2[1] == "D") { oo4 = 13; }
+            if (oo2[1] == "E") { oo4 = 14; }
+            if (oo2[1] == "F") { oo4 = 15; }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            string bufftext = textBox2.Text;
+            string[] bufftexts = bufftext.Split(' ');
+            string lung1 = bufftext.Replace(" ", "");
+            int lung = (lung1.Length) / 2;
+            intbufftext = new int[lung];
+
+            foreach (string oo in bufftexts) {
+                var oostringbuilder = new StringBuilder(oo);
+                oostringbuilder.Insert(1, ".");
+                string oo1 = oostringbuilder.ToString();
+
+                oo2 = oo1.Split('.');
+                if (oo2[0] == "A" || oo2[0] == "B" || oo2[0] == "C" || oo2[0] == "D" || oo2[0] == "E" || oo2[0] == "F")
+                {
+                    konversiabjadhexa1();
+                }
+                else {
+                    oo3 = Convert.ToInt32(oo2[0]);
+                }
+                if (oo2[1] == "A" || oo2[1] == "B" || oo2[1] == "C" || oo2[1] == "D" || oo2[1] == "E" || oo2[1] == "F")
+                {
+                    konversiabjadhexa2();
+                }
+                else
+                {
+                    oo4 = Convert.ToInt32(oo2[1]);
+                }
+                int oo5 = (oo3 * 16) + oo4;
+                intbufftext[iter] = oo5;
+                iter = iter + 1;
+            }
+            backgroundWorker2.RunWorkerAsync();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (button5.Text == "Connect")
+            {
+                button5.Text = "Close";
+                String port = comboBox1.Text;
+                serial_port_connect(port);
+                seport.Open();
+                label4.Text = "Connected to "+port;
+                backgroundWorker1.RunWorkerAsync();  //start receiving data in background
+                backgroundWorker2.WorkerSupportsCancellation = true; // ability to cancel this thread
+            }
+            else
+            {
+
+                if (button5.Text == "Close") 
+                {
+                    seport.Close();
+                    label4.Text = "not connected";
+                    button5.Text = "Connect";
+                }
+            }
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            while (seport.IsOpen)
+            {
+                try
+                {
+                    // int transmitCount = transmittxtbox.TextLength;
+                    int a = seport.ReadByte();
+                    string strTemp = "";
+                    string result2 = string.Empty;
+                    String resultString = a.ToString("X2");
+                    DateTime dt = DateTime.Now;
+                    String dtn = dt.ToShortTimeString();
+
+                    if (formnya == 0)
+                    {
+                        // this.textBox1.Invoke(new MethodInvoker(delegate () { textBox1.AppendText(resultString + " "); }));
+                        result2 = resultString + " ";
+                        result3 += result2;
+                    }
+                    if (formnya == 1)
+                    {
+                        this.textBox1.Invoke(new MethodInvoker(delegate () { textBox1.AppendText(resultString + " "); }));
+                        result2 = resultString + " ";
+                        result3 += result2;
+                    }
+
+                }
+                catch (Exception x)
+                {
+                    MessageBox.Show(x.Message.ToString());
+                }
+            }
+        }
+
+        private void backgroundWorker2_DoWork(object sender, DoWorkEventArgs e)
+        {
+            
+            {
+               // seport.Write(bufftext);
+            
+                int count1 = 0;
+                foreach (int a in intbufftext)
+                {
+                    if (seport.IsOpen)
+                    {
+
+                        //int a = STR.Read();
+                        //String resultString = new string(a.ToString("X").ToCharArray());
+
+                        byte[] bytCAT = BitConverter.GetBytes(intbufftext[count1]);
+                        seport.Write(bytCAT, 0, 1);
+                        count1 = count1 + 1;
+                        iter = 0;
+                        
+                        string timestamps = DateTime.Now.ToString("HH:mm:ss");
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Send Failed");
+                    }
+                    backgroundWorker2.CancelAsync();
+                }
+            }
+        }
+    }
+}
